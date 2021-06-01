bfba13f83d03581761de1032f5d561b8d7dae740
mathi <mathiaswrc@gmail.com>
2021-06-01 09:12:22 -0300

    done gulpfile | tested gulp

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,36 +1,51 @@
+// Adiciona os modulos instalados
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
 
+//Função para compilar SASS e adicionar prefixos
 function compilaSass() {
   return gulp
-  .src('css/scss/*.scss')
-  .pipe(sass({
-    outputStyle: 'compressed'
-  }))
-  .pipe(autoprefixer({
-    overrideBrowserslist: ['last 2 versions'],
-    cascade: false
-  }))
-  .pipe(gulp.dest('css/'))
+    .src('css/scss/*.scss')
+    .pipe(
+      sass({
+        outputStyle: 'compressed',
+      }),
+    )
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ['last 2 versions'],
+        cascade: false,
+      }),
+    )
+    .pipe(gulp.dest('css/'))
+    .pipe(browserSync.stream());
 }
 
+// Tarefa gulp para a função de SASS
 gulp.task('sass', compilaSass);
 
-function browser(){
+// Função para iniciar o browser
+function browser() {
   browserSync.init({
     server: {
-      baseDir: "./"
-    }
+      baseDir: './',
+    },
   });
 }
 
+// Tarefa para iniciar o browser-sync
 gulp.task('browser-sync', browser);
 
+// Função watch do gulp
 function watch() {
   gulp.watch('css/scss/*.scss', compilaSass);
+  gulp.watch(['*.html', '*.js']).on('change', browserSync.reload);
 }
 
-gulp.task('default', watch);
+// Tarefa para iniciar o watch
+gulp.task('watch', watch);
 
+// Tarefa padrão do gulp para iniciar watch e browser-sync
+gulp.task('default', gulp.parallel('watch', 'browser-sync'));
